95c8adae94e2ed062679af5c5e2a273ba334c10f
Lahiru-Madhuwantha <lahirumadhuwantha293@gmail.com>
2023-08-17 02:18:34 +0530

    Falling

--- Even-Up-Solitare.cpp
+++ Even-Up-Solitare.cpp
@@ -8,15 +8,15 @@ using namespace std;
 int main(){
 
   int n;
-  cin >> n;
+  cin >> n; // Array size
 
-  vector<int> array;
-  vector<int> remaining;
+  vector<int> array; // Array ek denne vector ekk vidiyt.Ethakota mulinm array eke size ek denda one na
+  vector<int> remaining; // Ithuruwa dana array ek
 
   for(int i = 0; i < n; i++){
     int card ;
-    cin >> card;
-    array.push_back(card);
+    cin >> card;       
+    array.push_back(card); // Array ek ta element denwa
   }
   for(int card:array){
     if(!remaining.empty() && ((remaining.back()+ card)%2== 0)){
@@ -26,6 +26,7 @@ int main(){
       remaining.push_back(card);
     }
   }
+
   cout << remaining.size() << endl;
   return 0;
 
